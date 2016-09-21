65022906429c342c58f57849d11cba9be17e6bfe
root <root@beaglebone.localdomain>
2016-09-21 18:40:08 -0400

    bug fixes on linux

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 
 (setq custom-file "~/.emacs.d/personal/custom.el")
 (load custom-file 'noerror)
+(load "~/.emacs.d/package.el")
 
 (load "~/.emacs.d/personal/defuns")
 
@@ -31,7 +32,7 @@
 (personal 'zoom)
 (personal 'utf-8)
 ;(personal 'irony) ;;unused
-
+(message "Personal *.el loaded succesfully...")
 ;; elpa managed
 ;; ------------------
 (require 'package)
@@ -50,39 +51,40 @@
 (package 'nlinum)
 (package 'exec-path-from-shell)
 (package 'flx-ido)
-(package 'flx-isearch)
-(package 'haml-mode)
-(package 'htmlize)
 ;(package 'js2-mode)
 ;(package 'irony)
-(package 'lua-mode)
-(package 'markdown-mode)
+
 (package 'maxframe)
 (package 'motion-mode)
 (package 'multiple-cursors)
-(package 'powerline)
-(package 'projectile)
-(package 'sass-mode)
-(package 'shell-pop)
-(package 'smartparens)
-(package 'toggle-quotes)
 (package 'textile-mode)
-(package 'yaml-mode)
+(package 'toggle-quotes)
 (package 'yasnippet)
 (package 'auto-complete)
 (package 'auto-complete-c-headers)
-
 (personal 'auto-complete-c++)
 (personal 'yasnippet)
-(personal 'smartparens)
+
 
 (if (>= emacs-major-version 24)
     (progn
       ;; Do something for Emacs 24 or later
       (personal 'theme)
+      (package 'lua-mode)
+      (package 'flx-isearch)
+      (package 'haml-mode)
+      (package 'htmlize)
+      (package 'markdown-mode)
       (personal 'magit)
       (package 'magit)
       (package 'magit-gh-pulls)
+      (package 'sass-mode)
+      (package 'smartparens)
+      (personal 'smartparens)
+      (package 'yaml-mode)
+      (package 'powerline)
+      (package 'projectile)
+      (package 'shell-pop)
       )
   ;; Do something else for Emacs 23 or less
   )
@@ -97,16 +99,12 @@
   )
  ((eq system-type 'gnu/linux)
   ;; Do something on GNU/Linux
-  )
- ;; ...
- (t
-  ;; Do something in any other case
   ))
 
-;; Showing the line numbers for all files
+;;Showing the line numbers for all files
 (global-linum-mode t)
-;; submodule managed
-;; ------------------
+;;submodule managed
+;;------------------
 (add-to-list 'load-path "~/.emacs.d/vendor/")
 (vendor 'llvm-mode)
 ;; To add copy-paste feature in Macs
@@ -115,5 +113,5 @@
 ;; (vendor 'key-chord)     ;; unused
 (vendor 'mode-line-bell)
 (vendor 'insert-time   'insert-time 'insert-date 'insert-date-time 'insert-personal-time-stamp)
-(vendor 'electric-align 'electric-align-mode)
+;;(vendor 'electric-align 'electric-align-mode)
 (add-hook 'prog-mode-hook 'electric-align-mode)
