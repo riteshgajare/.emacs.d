f2a0c591b572c498f4a0eb304761bd72ddfe24fa
rgajare <rgajare@ncsu.edu>
2016-11-06 15:50:37 -0500

    compilation auto scroll

--- personal/c.el
+++ personal/c.el
@@ -5,3 +5,13 @@
  ;; Non-nil means display source file containing the main routine at startup
  gdb-show-main t
  )
+
+;;(add-to-list 'auto-mode-alist '("Makefile" . whitespace-mode)
+;(add-to-list 'load-path "/path/to/flycheck-clangcheck") ;; if you installed manually
+
+;;(require 'fic-mode)
+(add-hook 'c++-mode-hook 'turn-on-fic-mode)
+(add-hook 'c-mode-hook 'turn-on-fic-mode)
+
+;; Compilation output
+(setq compilation-scroll-output t)
